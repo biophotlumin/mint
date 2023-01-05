55a5b18fa4f45ab0c2f69b38ca689cc6a2c790af
BptGrm <83828302+BptGrm@users.noreply.github.com>
2023-01-05 14:48:51 +0100

    Delete archive directory

--- archive/repro.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-utp = np.load('/home/baptiste/Documents/utp/post.npy')
-upd = np.load('/home/baptiste/Documents/upd/post.npy')
-#upd_pandas = np.load('/home/baptiste/Documents/upd_pandas/post.npy')
-#upd_np = np.load('/home/baptiste/Documents/upd_np/post.npy')
-
-#print(utp-upd)
-print(utp-upd)
-print(np.__version__)
